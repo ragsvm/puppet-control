070c270131df9f41ae64991cae3d8f40d30ae832
rags ubu lap <rags@me.com>
2019-06-15 09:37:45 +0530

    modded site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,9 @@
 File { backup => false }
-node 'ip-172-31-12-123' {
-  notify {'centosnode':
-   message => 'This is ip-172-31-12-123 using a node def in sitepp'
-  }
-}
+#node 'ip-172-31-12-123' {
+#  notify {'centosnode':
+#   message => 'This is ip-172-31-12-123 using a node def in sitepp'
+#  }
+#}
 node 'ip-172-31-27-245.ap-south-1' {
   notify {'matched the chopped hostname':}
   include pe_repo::platform::el_6_x86_64
